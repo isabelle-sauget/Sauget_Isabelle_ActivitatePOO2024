74478e6d22752c27d9a8e9ee443994c1f2954023
isabelle-sauget <saugetisabelle23@stud.ase.ro>
2024-10-30 21:14:52 +0200

    seminar 4 pe baza codului de la sem 3

--- Seminar3.cpp
+++ Seminar3.cpp
@@ -6,9 +6,8 @@ using namespace std;
 class Cofetarie {
 
 private:
-	string nume;
-	int nrAngajati;
-	float* salarii;
+	string nume;//
+	int nrAngajati;//
 	bool esteVegana;
 	const int anDeschidere;
 	float adaos;
@@ -23,9 +22,8 @@ public:
 	{
 		
 		this->nume = "Delice";
-		this->nrAngajati = 0;
+		this->nrAngajati = 3;
 		this->esteVegana = true;
-		this->salarii = NULL;
 		
 	}
 
@@ -39,40 +37,9 @@ public:
 		this->nrAngajati = nrAngajati;
 		this->esteVegana = vegan;
 		this->adaos = adaos;
-		this->salarii = new float[nrAngajati];
-		for (int i = 0; i < nrAngajati; i++)
-		{
-			this->salarii[i] = 1000 + 100 * i;
-		}
 		
 
 	}
-
-	Cofetarie(const Cofetarie& c): anDeschidere(c.anDeschidere)
-	{
-		this->nume = c.nume;
-		this->nrAngajati = c.nrAngajati;
-		this->salarii = new float[c.nrAngajati];
-		for (int i = 0; i < c.nrAngajati; i++)
-		{
-			this->salarii[i] = c.salarii[i];
-		}
-		this->esteVegana = c.esteVegana;
-		this->adaos = c.adaos;
-	}
-
-
-	~Cofetarie() {
-
-		//se sterge ce alocam in heap!
-		if (this->salarii != NULL)
-		{
-			delete[]this->salarii;
-
-		}
-
-	}
-
 
 	string getNume()
 	{
@@ -84,42 +51,6 @@ public:
 		if (nume.length() > 2)
 		{
 			this->nume = nume;
-		}
-	}
-
-	int getNrAngajati()
-	{
-		return this->nrAngajati;
-	}
-
-	void setNrAngajati(int nrAngajati, float *salarii)
-	{
-		if (nrAngajati > 0)
-		{
-			this->nrAngajati = nrAngajati;
-
-			if (this->salarii != NULL)
-			{
-				delete[]this->salarii;
-			}
-			this->salarii = new float[this->nrAngajati];
-			for (int i = 0; i < nrAngajati; i++)
-			{
-				this->salarii[i] = salarii[i];
-			}
-		}
-	}
-
-	float* getSalarii()
-	{
-		return this->salarii;
-	}
-
-	float getSalariu(int index)
-	{
-		if (index >= 0 && index < this->nrAngajati)
-		{
-			return this->salarii[index];
 		}
 	}
 
@@ -152,13 +83,13 @@ public:
 		return suma;
 	}
 
-	
+
+	Cofetarie operator=(const Cofetarie& c)
+	{
+
+	}
 
 };
-
-void f(Cofetarie c) {
-
-}
 
 int Cofetarie::TVA = 9;
 
@@ -195,27 +126,18 @@ int main()
 
 
 	Cofetarie c;
+
+	cout << c.getNume() << endl;
 	c.setNume("");
 	cout << c.getNume() << endl;
 	c.setNume("Alt nume");
 	cout << c.getNume() << endl;
-	c.setNrAngajati(4, new float[4] {2, 6, 4, 9});// de ce nu e recomandata initializarea inline?
-
-	Cofetarie c2("Minimal", 3, false, 2020, 10);
-	cout << c2.getSalarii()[1] << endl;
-	cout << c2.getSalariu(1) << endl;
 
 
-	c.setNrAngajati(5, new float[5] {1,2,3,4,5});
-	cout << c2.getSalariu(4);
 
 
-	//apelare constructor de copiere
 
-	Cofetarie c3(c2);
-	Cofetarie c4 = c2;
 
-	f(c4); //daca punem breakpoint observam ca este intai si intai apelat de constructorul de copiere
 
 
 
