b0fea2f6b0066dd5d0eb960fccbf132a0128433b
isabelle-sauget <saugetisabelle23@stud.ase.ro>
2024-11-07 00:17:27 +0200

    tema optionala 3 finalizata

--- tema_opt_2.cpp
+++ tema_opt_2.cpp
@@ -47,6 +47,12 @@ public:
 		this->esteCilindric = false;
 	}
 
+	Ghiveci(const Ghiveci& g) :volum(g.volum) 
+	{
+		this->material = g.material;
+		this->esteCilindric = g.esteCilindric;
+	}
+
 
 	 void static AfisareG(const Ghiveci& ghiveci )
 	{
@@ -64,23 +70,40 @@ public:
 		Ghiveci::AfisareG(ghiveci);
 	}
 
+
+	 string getMaterial()
+	 {
+		 return this->material;
+	 }
+
+	 void setMaterial(string material)
+	 {
+		 if (material.length() > 1)
+		 {
+			 this->material = material;
+		 }
+	 }
+
+
 };
 
 
 class Fertilizant {
 
-public:
+private:
 	bool esteOrganic;
 	static float concentratiePotasiu;
 	const int anulExpirarii;
 	int *pret;
+
+public: 
 
 	Fertilizant() :
 		anulExpirarii(2027)
 	{
 		this->esteOrganic = true;
 		this->pret = new int [3];
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < 3; i++)
 		{
 			this->pret[i] = i + i * 2;
 		}
@@ -91,7 +114,7 @@ public:
 		anulExpirarii(2025)
 	{
 		this->esteOrganic = false;
-		this->pret = new int[4];
+		this->pret = new int[3];
 		for (int i = 0; i < 3; i++)
 		{
 			this->pret[i] = i + i * 3;
@@ -103,15 +126,45 @@ public:
 	{
 		this->esteOrganic = false;
 		this->pret = new int[3];
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < 3; i++)
 		{
 			this->pret[i] = i +3;
 		}
 	}
 
+	Fertilizant(const Fertilizant& f):
+		anulExpirarii(f.anulExpirarii)
+	{ 
+		this->esteOrganic = f.esteOrganic;
+		this->pret = new int[3];
+		for (int i = 0; i < 3; i++)
+		{
+			this->pret[i] = f.pret[i];
+		}
+
+	}
+
+
 	~Fertilizant() {
 		if (this->pret)
 			delete[]this->pret;
+	}
+
+	static float getConcentratiePNoua()
+	{
+		return concentratiePotasiu;
+	}
+
+	static void setConcentratiePNoua(float cpnoua)
+	{
+		if (concentratiePotasiu > 0)
+		{
+			concentratiePotasiu = cpnoua;
+		}
+		else
+		{
+			cout << "Concentratia de potasiu nu poate fi negativa." << endl;
+		}
 	}
 
 
@@ -137,15 +190,40 @@ public:
 
 	}
 
+	int getPret()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			return this->pret[i];
+		}
+	}
+
+	void setPret(int *pret)
+	{
+			if (this->pret != NULL)
+			{
+				delete[]this->pret;
+			}
+			this->pret = new int[3];
+			for (int i = 0; i < 3; i++)
+			{
+				this->pret[i] = pret[i];
+			}
+	}
+
+	friend int PretMediu(const Fertilizant& f);
+
 };
 
 class Seminte {
 
-public:
+private:
 	string speciePlanta;
 	static int cantitate;
 	const int timpPanaLaRasadire;
 	float * dimensiune;
+
+public:
 
 	Seminte() :
 		timpPanaLaRasadire(15)
@@ -169,6 +247,13 @@ public:
 		this->dimensiune = new float(dimens);
 	}
 
+	Seminte(const Seminte& s):
+		timpPanaLaRasadire(s.timpPanaLaRasadire)
+	{
+		this->speciePlanta = s.speciePlanta;
+		this->dimensiune = new float(0.05f);
+	}
+
 	
 	~Seminte()
 	{
@@ -188,20 +273,35 @@ public:
 
 	
 
-	static int catePliculete( int nrSeminteCeruteSpreVanzare)
+	static int catePliculete1( int nrSeminteCeruteSpreVanzare)
 	{
 		return nrSeminteCeruteSpreVanzare / Seminte::cantitate;
 	}
 
-
-	
+	friend int catePliculete2(int nrSeminteCeruteSpreVanzare);
 
 
 };
 
+ int catePliculete2(int nrSeminteCeruteSpreVanzare)
+{
+	 return nrSeminteCeruteSpreVanzare / Seminte::cantitate;
+}
+
 string Ghiveci::culoare = "maro";
 float Fertilizant::concentratiePotasiu = 2.4f;
 int Seminte::cantitate = 100;
+
+int PretMediu(const Fertilizant& f)
+{
+	int sumaPreturi = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		sumaPreturi += f.pret[i];
+	}
+	return sumaPreturi / 3;
+}
+
 
 int main()
 {
@@ -212,14 +312,17 @@ int main()
 	Ghiveci* pointerG;
 	pointerG = new Ghiveci("lemn", false, 4);
 	pointerG->AfisareG(*pointerG);
+	cout << endl;
 
 	Fertilizant* pointerF;
 	pointerF = new Fertilizant(true, 2028);
 	pointerF->AfisareF();
+	cout << endl;
 
 	Seminte* pointerS;
 	pointerS = new Seminte("scadens micans", 20, 0.2);
 	pointerS->AfisareS();
+	cout << endl;
 
 	delete pointerG;
 	delete pointerF;
@@ -229,37 +332,60 @@ int main()
 
 	Ghiveci g1;
 	g1.AfisareG(g1);
+	cout << endl;
 	g1.modificaCuloare("alb",g1);
 
 	Fertilizant f1;
 	f1.AfisareF();
+	cout << endl;
 	(f1.potrivitPentruOrhidei() ? cout << " Este potrivit pentru orhidei." << endl : cout << "Nu este potrivit pentru orhidei." << endl);
 
 	Seminte s1;
 	s1.AfisareS();
-	cout << "Numarul necesar de pliculete pentru "<< nrSeminteCeruteSpreVanzare <<" seminte este de:  " << s1.catePliculete(nrSeminteCeruteSpreVanzare) << endl;
+	cout << endl;
+	cout << "Numarul necesar de pliculete pentru "<< nrSeminteCeruteSpreVanzare <<" seminte este de:  " << s1.catePliculete1(nrSeminteCeruteSpreVanzare) << endl;
+
+	//apelare functie prietena
+
+	cout << " Functie prietena: " << catePliculete2(nrSeminteCeruteSpreVanzare) << endl;
 	
 	//exemplu apelare constructor 2 cu 2 parametri
 
 	Ghiveci g2("plastic", 15);
 	g2.AfisareG(g2);
+	cout << endl;
 	
 	Fertilizant f2(false,2028);
 	f2.AfisareF();
+	cout << endl;
 
 	Seminte s2("phalaenopsis",20);
 	s2.AfisareS();
+	cout << endl;
 
 	//exemplu apelare constructor 3 cu 3 parametri
 
+
 	Ghiveci g3("rasina", true, 35);
 	g3.AfisareG(g3);
+	cout << endl;
+	cout << "Materialul este: " << g3.getMaterial() << endl;
+	g3.setMaterial("portelan");
+	cout << "Noul material este: " << g3.getMaterial() << endl;
+
 
 	Fertilizant f3(true, 2024, 45);
 	f3.AfisareF();
+	cout << endl;
+	cout << "Preturile sunt: " << f3.getPret() << endl;
+	int pretNou[3] = { 17,19,34 };
+	f3.setPret(pretNou);
+	cout << "Preturile noi sunt: " << f3.getPret() << endl;
+
 
 	Seminte s3("scindapsus pictus", 5, 0.25);
 	s3.AfisareS();
+	cout << endl;
 
 	
 
